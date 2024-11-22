3f8b83b2eff1ae66e85b4e08bcca1bb0390f2053
Frossal <156116218+Frossal@users.noreply.github.com>
2024-11-22 05:37:36 -0500

    Add files via upload

--- Backend_Software/backend/urls.py
+++ Backend_Software/backend/urls.py
@@ -2,6 +2,8 @@
 from django.contrib import admin  
 from django.urls import path, include
 from django.http import HttpResponse
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Vista para la página principal
 def home_view(request):
@@ -11,4 +13,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),  # Esta es la línea que causaba el error
     path('api/', include('curso.urls')),  # Incluir las rutas de la API de cursos
     path('', home_view),  # Vista para la página raíz
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
